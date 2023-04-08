a865d933be48b3a1c28466fe5af2912dc6904492
Ashu912 <ashutosh.19cs327@citranchi.ac.in>
2023-04-08 21:31:20 +0530

    Succes

--- inde.js
+++ inde.js
@@ -13,6 +13,12 @@ function Add() {
    data = data - 1;
     document.getElementById("demo").innerText = data;
 }function Reset(){
-  data = data;
+  data = 0;
   document.getElementById("demo").innerText=0;
+}function Increment5(){
+  data =data+5;
+  document.getElementById("demo").innerText=data;
+}function Decrement10(){
+  data =data-10;
+  document.getElementById("demo").innerText=data;
 }
